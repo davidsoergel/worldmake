1b09dcd4a8a16ac4d9b9371defbdacd69f11efcd
David Soergel <dev@davidsoergel.com>
2013-08-27 11:38:26 -0400

    implicits for constant derivations.  Factor out hashing.

--- src/main/scala/worldmake/lib/MercurialWorld.scala
+++ src/main/scala/worldmake/lib/MercurialWorld.scala
@@ -1,22 +1,21 @@
 package worldmake.lib
 
-import scalax.file.Path
 import edu.umass.cs.iesl.scalacommons.util.Hash
-import worldmake.lib.MercurialWorkspaces
-import worldmake.{Derivation, ExternalPathDerivation}
+import worldmake.{WorldMakeConfig, Derivation, ExternalPathDerivation}
+import WorldMakeConfig.WMHash
 
 /**
  * @author <a href="mailto:dev@davidsoergel.com">David Soergel</a>
  */
 abstract class MercurialWorld {
-  
+
   protected val reposRequestedVersions: Map[String, (String, String)]
-  
+
   private val reposActualVersions: Map[String, String] = reposRequestedVersions.map({
     case (k, (b, "latest")) => (k, MercurialWorkspaces.getLatestVersions(k)(b))
     case (k, (b, v)) => (k, v)
   })
-  
+
   val workingDirs: Map[String, ExternalPathDerivation] = reposActualVersions.map({
     case (k: String, v: String) => (k, MercurialWorkspaces.get(k, v))
   })
@@ -24,6 +23,6 @@ abstract class MercurialWorld {
   val inputHash = Hash.toHex(WMHash(reposActualVersions.map({
     case (k: String, v: String) => k + v
   }).mkString))
-  
-  val targets:Map[String,Derivation[_]]
+
+  val targets: Map[String, Derivation[_]]
 }
